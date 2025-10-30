a95acce1a4ca08074483e657aa51d6d741578c29
12234626 <pjhalpha@inha.edu>
2025-10-30 22:45:28 +0900

    feat: 앱 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const appConfig = configService.get('app');
+  const logger = new Logger('Bootstrap');
+
+  app.enableCors({
+    credentials: true,
+  });
+  app.setGlobalPrefix('api');
+  app.useBodyParser('json', { limit: '50mb' });
+  app.useBodyParser('urlencoded', { extended: true, limit: '50mb' });
+  app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useStaticAssets('public', { prefix: '/public/' });
+
+  await app.listen(appConfig.port, '0.0.0.0');
+
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
