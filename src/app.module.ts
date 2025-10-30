1521fc1ff5461af61bf264c813ca82b2d0497db1
12234626 <pjhalpha@inha.edu>
2025-10-30 23:42:29 +0900

    feat: TypeORM 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import appConfig from './config/app.config';
+import typeormConfig from './config/db.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig],
+      load: [appConfig, typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('db'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
     }),
   ],
   controllers: [AppController],
