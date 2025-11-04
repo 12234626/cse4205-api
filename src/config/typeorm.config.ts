d9d971a5c1cee77c61196d67fdd393ac43bd351e
12234626 <pjhalpha@inha.edu>
2025-11-05 01:42:26 +0900

    feat: TypeORM SSL 설정 추가

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,4 +7,5 @@ export default registerAs('typeorm', () => ({
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_DATABASE || 'postgres',
+  ssl: process.env.DB_SSL === 'true',
 }));
