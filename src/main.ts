ceaa96e267ec24621c5e12262421d01c591bfba9
12234626 <pjhalpha@inha.edu>
2025-12-04 22:06:25 +0900

    fix: validation 오류 처리 방식 수정

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
           return Object.values(error.constraints || {}).join(', ');
         });
 
-        return ResponseException.validationError(messages.join('; '));
+        throw ResponseException.validationError(messages.join('; '));
       },
     }),
   );
