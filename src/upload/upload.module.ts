295258a5c406e512f711552b79c740236c75f804
leunein <ymkim2079@gmail.com>
2025-12-09 20:06:47 +0900

    feat: s3에 저장된 사진 url db에 저장하는 api 추가

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { ConsentRequestImageEntity } from 'src/consent-request/entities/consent-request-image.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ConsentRequestImageEntity]), UserModule],
   controllers: [UploadController],
   providers: [UploadService],
   exports: [UploadService],
