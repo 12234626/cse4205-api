004f325c7be432b4c06d2d6184216714c7ba0f26
leunein <ymkim2079@gmail.com>
2025-12-13 21:18:48 +0900

    feat: 로그제거

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -2,7 +2,6 @@ import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
 import type { Request } from 'express';
-import { createHash } from 'crypto';
 
 import { ResponseException } from 'src/common/exceptions/response.exception';
 
@@ -19,28 +18,10 @@ export class ApiKeyGuard implements CanActivate {
     const validApiKey = this.configService.get<string>('INTERNAL_API_KEY');
 
     if (!validApiKey) {
-      console.log('[DEBUG API KEY] No valid API key in backend env!', {
-        validApiKey, // undefined일 경우 확인용
-        apiKey,
-        ip: request.ip,
-        xff: request.headers['x-forwarded-for'],
-      });
       throw ResponseException.unauthorized();
     }
 
     if (!apiKey || apiKey !== validApiKey) {
-      const hash = (str: string) =>
-        createHash('sha256').update(str).digest('hex').slice(0, 12);
-
-      console.log('[DEBUG API KEY] Incoming request failed API key check', {
-        apiKeyExists: !!apiKey,
-        apiKeyLen: apiKey?.length,
-        apiKeyFp: apiKey ? hash(apiKey) : null,
-        validApiKeyLen: validApiKey.length,
-        validApiKeyFp: hash(validApiKey),
-        ip: request.ip,
-        xff: request.headers['x-forwarded-for'],
-      });
       throw ResponseException.unauthorized();
     }
 
