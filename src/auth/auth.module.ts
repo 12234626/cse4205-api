259b6865a0cf1b8867fd86afe80c5f8fd71cffa4
12234626 <pjhalpha@inha.edu>
2025-12-13 16:33:01 +0900

    fix: AuthModule 의존성 배열 수정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './services/auth.service';
 import { TokenService } from './services/token.service';
 import { AuthController } from './auth.controller';
+import { UserQuestModule } from 'src/user-quest/user-quest.module';
 import { TokenEntity } from './entities/token.entity';
 
 @Global()
 @Module({
-  imports: [TypeOrmModule.forFeature([TokenEntity])],
+  imports: [TypeOrmModule.forFeature([TokenEntity]), UserQuestModule],
   controllers: [AuthController],
   providers: [AuthService, TokenService],
   exports: [AuthService, TokenService],
